04345c2bb3830152e5d727d87dd203298b704db7
Osman <osrbck@gmail.com>
2024-02-08 14:04:40 +0300

    update scripts updated

--- idari Modüller/update-scripts.sql
+++ idari Modüller/update-scripts.sql
@@ -12,6 +12,7 @@ update personel
 set giris_tarihi = to_date('01.01.2012', 'DD.MM.YYYY') 
 where yonetici_id = 919
 
+
 --------------------------------------------
 
 update personel set 
@@ -20,12 +21,34 @@ unvan = (select unvan from personel where personel_id = 5004)
 where personel_id = 5005
 
 --------------------------------------------
+--UPDATE JOIN
+--------------------------------------------
 
 update personel set 
 (maas, unvan)
 =
 (select maas, unvan from personel where personel_id = 5004)
 where personel_id = 5005
+
+
+--------------------------------------------
+
+update personel
+set maas = maas * 1.25
+where dept_id IN
+(
+select dept_id from departman
+where upper(dept_ismi) LIKE '%CRM%'
+)
+
+--------------------------------------------
+
+update iller i
+set bolge_adi = 
+(
+    select bolge_adi from bolgeler b
+    where i.bolge_kodu = b.bolge_kodu
+);
 
 
 --------------------------------------------
